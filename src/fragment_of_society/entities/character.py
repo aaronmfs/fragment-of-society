3e576fb101781f834257511da986b4deffb5e9ea
Aaron Mathew F. Sinay <sinay.aaronmathew@gmail.com>
2026-03-16 01:03:58 +0800

    refactor(Character): Add type hint for character.stats

--- src/fragment_of_society/entities/character.py
+++ src/fragment_of_society/entities/character.py
@@ -3,8 +3,8 @@ from fragment_of_society.components.stats import Stats
 
 class Character(Entity):
     def __init__(self,
-                 name:str,
-                 stats: Stats = Stats(10, 10, 10, 10,10, 0),
+                 name: str,
+                 stats: Stats,
                  speed: float = 650,
                  x: float = 0,
                  y: float = 0
@@ -13,3 +13,6 @@ class Character(Entity):
         super().__init__(stats, x, y)
         self.name = name
         self.speed = speed
+
+    def __repr__(self) -> str:
+        return f"Character({self.name}, hp={self.hp}/{self.stats.max_hp})"
